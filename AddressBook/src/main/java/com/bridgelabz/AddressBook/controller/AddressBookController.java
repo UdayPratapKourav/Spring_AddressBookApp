765b8ee8d5e4383f7f785b5ef9e9c21a3b2be4a5
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-03-08 12:14:48 +0530

    [Uday] Update. controller

--- AddressBook/src/main/java/com/bridgelabz/AddressBook/controller/AddressBookController.java
+++ AddressBook/src/main/java/com/bridgelabz/AddressBook/controller/AddressBookController.java
@@ -1,14 +1,23 @@
 package com.bridgelabz.AddressBook.controller;
 
+import com.bridgelabz.AddressBook.service.AddressBookServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/addressbook")
 public class AddressBookController {
-    @GetMapping("/hello")
+    private AddressBookServiceImpl addressBookService;
+    @Autowired
+    public AddressBookController(AddressBookServiceImpl addressBookService){
+        this.addressBookService=addressBookService;
+    }
+
+    //testing application working or not
+    @GetMapping("/test")
     public ResponseEntity<String> test(){
         return ResponseEntity.ok("Connected Successfully");
     }
